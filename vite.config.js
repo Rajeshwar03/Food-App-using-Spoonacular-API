151b143f78b271ac7a23dfcde53c5a11dce327a2
Rajeshwar03 <grrgangula52@gmail.com>
2025-05-24 08:05:48 +0530

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'css-module',
+      transform(code, id) {
+        if (id.endsWith('.module.css')) {
+          // Perform any desired transformations on the CSS code here
+          return {
+            code,
+            map: null, // Provide source maps if available
+          };
+        }
+      },
+    },
+  ],
+});
